7c567b55ce1ac01a3e247567e3d6682bc6b11445
saurabh <chauhansaurabh@2071gmail.com>
2026-06-30 07:34:32 +0530

    fix cors issue:

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,20 +6,32 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://interview-iq-eight-alpha.vercel.app"
+]
+
 app.use(cors({
-    origin: "https://interview-iq-eight-alpha.vercel.app",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
     credentials: true
 }))
 
-/* require all the routes here */
+
+// routes
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-/* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
-// Root route 👇
+
 app.get('/', (req, res) => {
     res.json({ message: 'InterviewIQ API is live 🚀' })
 })
